370eeb43301dfb773b6eda21a3278d644391f209
bdafahim <badrudduza20@student.sust.edu>
2020-01-22 17:10:53 +0600

    onChange added to PostForm

--- src/components/Postfoorm.js
+++ src/components/Postfoorm.js
@@ -7,7 +7,13 @@ class Postfoorm extends Component {
       title: "",
       body: ""
     };
+    this.onChange = this.onChange.bind(this);
   }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     return (
       <div>
@@ -16,13 +22,22 @@ class Postfoorm extends Component {
           <div>
             <label>Title: </label>
             <br />
-            <input type="text" name="title" value={this.state.title} />
+            <input
+              type="text"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+            />
           </div>
           <br />
           <div>
             <label>Body: </label>
             <br />
-            <textarea name="body" value={this.state.body} />
+            <textarea
+              name="body"
+              onChange={this.onChange}
+              value={this.state.body}
+            />
           </div>
           <br />
           <button type="submit">Submit</button>
