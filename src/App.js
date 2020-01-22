63252b95e20a416cf9e27481168b56e4166c42b5
bdafahim <badrudduza20@student.sust.edu>
2020-01-22 18:56:31 +0600

    Reducer added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
 import Posts from "./components/Posts";
 import Postfoorm from "./components/Postfoorm";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Postfoorm />
-          /<hr />
-          <Posts />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Postfoorm />
+            /<hr />
+            <Posts />
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
