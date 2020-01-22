982c2ab98cba880f6f27fd9e5947aec36b91e717
bdafahim <badrudduza20@student.sust.edu>
2020-01-22 13:31:21 +0600

    Boiler plate for Posts class

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,1 +1,15 @@
 import React, { Component } from "react";
+
+class Posts extends Component {
+  componentWillUnmount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>Hi there..</h1>
+      </div>
+    );
+  }
+}
+
+export default Posts;
