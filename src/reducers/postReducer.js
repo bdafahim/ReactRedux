306454143997bcf6f9ff2be3c0b792871c750425
bdafahim <badrudduza20@student.sust.edu>
2020-02-02 15:37:30 +0600

    Reducer created for comments

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,23 +1,29 @@
-import { FETCH_POSTS, NEW_POST } from "../actions/types";
+import { FETCH_POSTS, NEW_POST, FETCH_COMMENTS } from "../actions/types";
 
 const initialState = {
-  items: [],
-  item: {}
+    items: [],
+    item: {}
 };
 
 export default function(state = initialState, action) {
-  switch (action.type) {
-    case FETCH_POSTS:
-      return {
-        ...state,
-        items: action.payload
-      };
-    case NEW_POST:
-      return {
-        ...state,
-        items: action.payload
-      };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case FETCH_POSTS:
+            return {
+                ...state,
+                items: action.payload
+            };
+        case NEW_POST:
+            return {
+                ...state,
+                items: action.payload
+            };
+
+        case FETCH_COMMENTS:
+            return {
+                ...state,
+                items: action.payload
+            };
+        default:
+            return state;
+    }
 }
